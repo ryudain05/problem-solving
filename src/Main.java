cf8b10ab66cee9aaca7168df41de86416ca8a391
ryudain05 <dkdlsekdls@naver.com>
2023-09-18 23:32:41 +0900

    3주차:동전쌓기 - 자료구조2

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
+        String input = br.readLine();
+        String[] arr = input.split(" ");
 
-        boolean Jolly, sort;
-        int curr, diff;
+        Stack<Integer> stack = new Stack<>();
 
-
-        String s;
-
-
-        while((s = br.readLine()) != null) {
-            st = new StringTokenizer(s);
-
-            Jolly = true;
-            sort = true;
-
-            if (!st.hasMoreTokens()) {
-                Jolly = false;
-                break;
-            }
-
-
-            int N = Integer.parseInt(st.nextToken());
-
-            int prev = Integer.parseInt(st.nextToken());
-            int[] res = new int[N + 1];
-
-
-
-            for (int i = 1; i < N; i++) {
-                curr = Integer.parseInt(st.nextToken());
-                diff = Math.abs(curr - prev);
-
-
-                if (diff <= 0 || diff >= N || res[diff] == 1) {
-                    Jolly = false;
-                    break;
+        for (String s : arr) {
+            if (s.equals("*") && !stack.isEmpty()) {
+                stack.pop();
+            } else if (stack.isEmpty()) {
+                if (!s.equals("*"))
+                    stack.push(Integer.valueOf(s));
+            } else {
+                int coin = stack.peek();
+                if (coin >= Integer.valueOf(s)) {
+                    stack.push(Integer.valueOf(s));
                 }
-
-                if(curr <= prev) {
-                    sort = false;
-                }
-
-                res[diff] = 1;
-                prev = curr;
             }
-            if(Jolly) {
-                if(sort)System.out.println("Jolly");
-                else System.out.println("Little Jolly");
-            } else System.out.println("Not Jolly");
+        }
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
         }
     }
 }
