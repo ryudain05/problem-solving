98a7273420b1b9100f24a8e5a3ca5e879fa920e3
ryudain05 <dkdlsekdls@naver.com>
2023-09-19 23:16:36 +0900

    3주차: 동맹휴업 - 자료구조2

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = br.readLine();
-        String[] arr = input.split(" ");
+        int[] arr;
 
-        Stack<Integer> stack = new Stack<>();
+        int c, n, p, lost, count;
 
-        for (String s : arr) {
-            if (s.equals("*") && !stack.isEmpty()) {
-                stack.pop();
-            } else if (stack.isEmpty()) {
-                if (!s.equals("*"))
-                    stack.push(Integer.valueOf(s));
-            } else {
-                int coin = stack.peek();
-                if (coin >= Integer.valueOf(s)) {
-                    stack.push(Integer.valueOf(s));
+        c = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < c; i++) {
+            n = Integer.parseInt(br.readLine());
+            p = Integer.parseInt(br.readLine());
+
+            arr = new int[p];
+            List<String> list = new ArrayList<>();
+
+            for (int j = 0; j < p; j++) {
+                arr[j] = Integer.parseInt(br.readLine()); //휴업지수
+            }
+
+            lost = 0;
+            for (int day = 1; day <= n; day++) {
+                count = 0;
+                if(day%7==0||day%7==1) continue;
+
+                for (int N : arr) {
+                    if (day % N == 0) {
+                        count++;
+                    }
+                }
+
+                int num = (int) Math.round((double) p / 2);//과반수를 의미하는 것
+                if (count >= num) {//과반수를 넘었으면 얘는 공동휴일로 지정이 된거야
+                    String str = "";
+                    lost++;
+                    if (day % 7 == 2) {//월
+                        str += "Mon: ";
+                        str += String.valueOf(day);
+                    } else if (day % 7 == 3) {//화
+
+                        str += "Tue: ";
+                        str += String.valueOf(day);
+
+                    } else if (day % 7 == 5) {//목
+
+                        str += "Thu: ";
+                        str += String.valueOf(day);
+
+                    } else if (day % 7 == 6) {//금
+
+                        str += "Fri: ";
+                        str += String.valueOf(day);
+
+                    }
+                    if (!str.equals(""))
+                        list.add(str);
                 }
             }
-        }
-        while (!stack.isEmpty()) {
-            System.out.print(stack.pop() + " ");
+
+            System.out.println("Lost: " + lost);
+            for (String s : list)
+                System.out.println(s);
+
+            System.out.println();
         }
     }
 }
