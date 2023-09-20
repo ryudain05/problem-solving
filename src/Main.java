c395fe8207b562ed28820bf0c5335b77c94f4d24
ryudain05 <dkdlsekdls@naver.com>
2023-09-20 15:26:27 +0900

    3주차: 3번문제 - 자료구조3

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,40 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] arr;
+        String s;
 
-        int c, n, p, lost, count;
+        PriorityQueue<String> q = new PriorityQueue<>(Comparator.reverseOrder());
 
-        c = Integer.parseInt(br.readLine());
+        while (true) {
 
-        for (int i = 0; i < c; i++) {
-            n = Integer.parseInt(br.readLine());
-            p = Integer.parseInt(br.readLine());
+            s = br.readLine();
 
-            arr = new int[p];
-            List<String> list = new ArrayList<>();
-
-            for (int j = 0; j < p; j++) {
-                arr[j] = Integer.parseInt(br.readLine()); //휴업지수
+            if (s == null || s.isEmpty()) {
+                break;
             }
 
-            lost = 0;
-            for (int day = 1; day <= n; day++) {
-                count = 0;
-                if(day%7==0||day%7==1) continue;
+            if (!q.isEmpty() && s.equals("*")) {
+                q.poll();
+            } else q.offer(s);
 
-                for (int N : arr) {
-                    if (day % N == 0) {
-                        count++;
-                    }
-                }
 
-                int num = (int) Math.round((double) p / 2);//과반수를 의미하는 것
-                if (count >= num) {//과반수를 넘었으면 얘는 공동휴일로 지정이 된거야
-                    String str = "";
-                    lost++;
-                    if (day % 7 == 2) {//월
-                        str += "Mon: ";
-                        str += String.valueOf(day);
-                    } else if (day % 7 == 3) {//화
+        }
 
-                        str += "Tue: ";
-                        str += String.valueOf(day);
-
-                    } else if (day % 7 == 5) {//목
-
-                        str += "Thu: ";
-                        str += String.valueOf(day);
-
-                    } else if (day % 7 == 6) {//금
-
-                        str += "Fri: ";
-                        str += String.valueOf(day);
-
-                    }
-                    if (!str.equals(""))
-                        list.add(str);
-                }
-            }
-
-            System.out.println("Lost: " + lost);
-            for (String s : list)
-                System.out.println(s);
-
-            System.out.println();
+        while (!q.isEmpty()) {
+            System.out.println(q.poll());
         }
     }
 }
