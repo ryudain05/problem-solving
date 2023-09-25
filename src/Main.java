fb5e7d9ceaafed704e336b08b0094ec55e2a124f
ryudain05 <dkdlsekdls@naver.com>
2023-09-25 22:24:44 +0900

    4주: 문자열예제 1번문제

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,33 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
+        // 입력을 받기 위한 BufferedReader 생성
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        String N = br.readLine();
+        String M = br.readLine();
 
-        StringBuilder sb = new StringBuilder();
+        int[] countN = new int[26];
+        int[] countM = new int[26];
 
-        List<Integer> list = new ArrayList<>();
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
-            list.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < N.length(); i++) {
+            countN[N.charAt(i) - 'a']++;
         }
 
-        int count = 0;
-
-        for (int i = 1; i <= n; i++) {
-            boolean check = false;
-            if (!Two(i)) {
-                for (int j = 0; j < m; j++) {
-                    if (i % list.get(j) == 0) {
-                        check = true;
-                        break;
-                    }
-                }
-                if (!check) {
-                    sb.append(i).append(" ");
-                    count++;
-                    if (count >= 100) {
-                        break;
-                    }
-                }
-            }
+        for (int i = 0; i < M.length(); i++) {
+            countM[M.charAt(i) - 'a']++;
         }
 
-        if (sb.length() < 1) {
-            System.out.println("none");
-        } else System.out.print(sb.toString().trim());
+        int res = 0;
 
-    }
+        for (int i = 0; i < 26; i++) {
+            res += Math.abs(countN[i] - countM[i]);
+        }
 
-    static boolean Two(int i) {
-        return (i & (i - 1)) == 0;
+        System.out.println(res);
     }
 }
