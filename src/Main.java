a0688be91cb22c36f18147737dea054805918215
ryudain05 <dkdlsekdls@naver.com>
2023-09-28 23:40:49 +0900

    4주차: 문자열1-3번문제

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        String s = br.readLine().toUpperCase();
-        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
 
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == ' ') {
-                sb.append(" ");
+        StringBuilder answer = new StringBuilder();
+
+        int num = 0, start = 0, end = 1;
+
+
+        while (num < str.length()) {
+            String[] arr = new String[N];
+
+            for (int i = 0; i < N; i++) {
+
+                if (num + i >= str.length())
+                    break;
+
+                arr[i] = str.substring(num, num + i + 1);
             }
-            else if(N > 26) {
-                N = N % 26;
-                char ch = (char) (s.charAt(i) + N);
-                sb.append(ch);
+
+            ArrayList<String> tempList = new ArrayList<>(Arrays.asList(arr));
+            tempList.removeAll(Arrays.asList((Object) null));
+            arr = tempList.toArray(new String[tempList.size()]);
+
+            int[] repeat = new int[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                start = num + i + 1;
+                end = start + arr[i].length();
+                while (end <= str.length()) {
+                    if (str.substring(start, end).equals(arr[i])) {
+                        repeat[i]++;
+                        start += arr[i].length();
+                        end += arr[i].length();
+                    } else {
+                        break;
+                    }
+                }
             }
-            else {
-                char ch = (char) (s.charAt(i) + N);
-                if(ch > 'Z') {
-                    ch = (char) (ch - 26);
-                    sb.append(ch);
-                } else sb.append(ch);
+
+            int max = 0;
+            int maxToken = 0;
+
+            for (int i = 0; i < repeat.length; i++) {
+                if (max <= repeat[i]) {
+                    max = repeat[i];
+                    maxToken = i;
+                }
+            }
+
+            if (max == 0) {
+                answer.append(str.charAt(num)).append('1');
+                num++;
+            } else {
+                if (maxToken == 0) {
+                    answer.append(arr[0]).append(max + 1);
+                } else {
+                    answer.append(arr[maxToken].toUpperCase()).append(max + 1);
+                }
+                num += (max + 1) * (maxToken + 1);
             }
         }
 
-        System.out.println(sb.toString());
+        System.out.println(answer);
     }
 }
