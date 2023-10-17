0208bd8b73d78e915d1a481efb84e7898ea764a7
ryudain05 <dkdlsekdls@naver.com>
2023-10-17 22:26:27 +0900

    7주차 조합론 - 합집합

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,40 @@
-import java.util.Scanner;
-import java.math.BigInteger;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class Main{
 
-        while (true) {
-            String tempa = scanner.next();
-            String tempb = scanner.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder("1");
+        String[] tmp = sb.toString().split("");
 
-            if (tempa.equals("0") && tempb.equals("0")) {
-                break;
-            }
+        ArrayList<String> res = new ArrayList<>();
 
-            BigInteger a = new BigInteger(tempa);
-            BigInteger b = new BigInteger(tempb);
-            BigInteger fib1 = BigInteger.ONE;
-            BigInteger fib2 = BigInteger.ONE;
-            BigInteger fib = fib1.add(fib2);
+        for(int i = 0; i < n; i++) {
+            res.add(sb.toString()); //[1, 11, 12, 1121, ...]
+            tmp = sb.toString().split("");
+            String tk = tmp[0];
 
+            sb = new StringBuilder();
             int count = 0;
-
-            while (fib.compareTo(b) <= 0) {
-                if (fib.compareTo(a) >= 0) {
-                    count++;
+            for(int j = 0; j < tmp.length; j++) {
+                if(tk.equals(tmp[j])) count++;
+                else {
+                    sb.append(tk + "" + count);
+                    tk = tmp[j];
+                    count=1;
                 }
 
-                fib1 = fib2;
-                fib2 = fib;
-                fib = fib1.add(fib2);
             }
-
-            System.out.println(count);
+            if(count>=1) {
+                sb.append(tk+""+count);
+                count=1;
+            }
         }
+        System.out.println(res.get(n-1));
     }
+
 }
