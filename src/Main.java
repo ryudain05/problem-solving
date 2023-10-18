85336e8ccd4ef88cdac41ac27f9946a2356cd431
ryudain05 <dkdlsekdls@naver.com>
2023-10-18 22:42:51 +0900

    7주차: 조합론 실습문제3번 피보나치수응용

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,59 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder("1");
-        String[] tmp = sb.toString().split("");
+        int N = Integer.parseInt(br.readLine());
 
-        ArrayList<String> res = new ArrayList<>();
-
-        for(int i = 0; i < n; i++) {
-            res.add(sb.toString()); //[1, 11, 12, 1121, ...]
-            tmp = sb.toString().split("");
-            String tk = tmp[0];
-
-            sb = new StringBuilder();
-            int count = 0;
-            for(int j = 0; j < tmp.length; j++) {
-                if(tk.equals(tmp[j])) count++;
-                else {
-                    sb.append(tk + "" + count);
-                    tk = tmp[j];
-                    count=1;
-                }
-
-            }
-            if(count>=1) {
-                sb.append(tk+""+count);
-                count=1;
-            }
+        for (int i = 0; i < N; i++) {
+            int M = Integer.parseInt(br.readLine());
+            List<Integer> list = fibo(M);
+            System.out.println(sum(list, M));
         }
-        System.out.println(res.get(n-1));
     }
 
+    static List<Integer> fibo(int m) {
+        List<Integer> list = new ArrayList<>();
+
+        int a = 1;
+        int b = 2;
+        int c;
+
+        list.add(a);
+        list.add(b);
+
+        while (true) {
+            c = a + b;
+            if (c <= m) {
+                list.add(c);
+            } else {
+                break;
+            }
+            a = b;
+            b = c;
+        }
+
+        Collections.sort(list, Collections.reverseOrder());
+        return list;
+    }
+
+    static int sum(List<Integer> fibo, int m) {
+        int sum = 0;
+
+        for (int num : fibo) {
+            if (num <= m) {
+                sum += num * num;
+                m -= num;
+            }
+            if (m == 0) {
+                break;
+            }
+        }
+        return sum;
+    }
 }
