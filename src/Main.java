191fd83600d2e5e4e007786e950dfa22a8fd186c
ryudain05 <dkdlsekdls@naver.com>
2023-11-01 20:47:39 +0900

    9주차: 백트래킹 - 1번문제

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,66 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int[][] miro = new int[4][4];
+    static boolean[][] path = new boolean[4][4];
+    static int[] dx = {0, 1}; // 오른쪽, 아래쪽
+    static int[] dy = {1, 0}; // 오른쪽, 아래쪽
 
-    static int count = 0;
-    static int n;
-    static int[] board;
+    static StringBuilder sb = new StringBuilder();
 
-    // 유망한지 판단하는 함수
-    static boolean promising(int cdx) {
-        // 같은 열이면 안되고, 대각선상에 있어서도 안 된다.
-        for (int i = 0; i < cdx; i++) {
-            if (board[cdx] == board[i] || cdx - i == Math.abs(board[cdx] - board[i])) {
-                return false;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 4; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 4; j++) {
+                miro[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        return true;
+
+        BFS(0, 0);
+        printPath();
+        System.out.println(sb);
     }
 
-    // nqueen 알고리즘 수행
-    static void nqueen(int cdx) {
-        // cdx가 마지막 행까지 수행하고 여기까지 오면, 찾기 완료
-        if (cdx == n) {
-            count++;
-            printBoard(); // 현재 퀸의 배치를 출력
-            return;
-        }
+    static void BFS(int startX, int startY) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{startX, startY});
+        path[startX][startY] = true;
 
-        for (int i = 0; i < n; i++) {
-            board[cdx] = i; // cdx번째 행, i번째 열에 queen을 놓아본다.
-            // 이후 그 행에 둔 것에 대한 유망성을 판단한다.
-            if (promising(cdx)) { // 그 자리에 두어도 괜찮았다면,
-                nqueen(cdx + 1); // 그 다음 행에 대해 퀸을 놓는 것을 해 본다.
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+
+
+            if (y + 1 < 4 && miro[x][y + 1] == 1 && !path[x][y + 1]) {
+                queue.offer(new int[]{x, y + 1});
+                path[x][y + 1] = true;
+            }
+
+            else if (x + 1 < 4 && miro[x + 1][y] == 1 && !path[x + 1][y]) {
+                queue.offer(new int[]{x + 1, y});
+                path[x + 1][y] = true;
             }
         }
     }
 
-    // 현재 퀸의 배치를 출력하는 함수
-    static void printBoard() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i] == j) {
-                    System.out.print("Q ");
+    static void printPath() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (path[i][j]) {
+                    sb.append("1").append(" ");
                 } else {
-                    System.out.print("* ");
+                    sb.append("0").append(" ");
                 }
             }
-            System.out.println();
+            sb.append("\n");
         }
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        board = new int[n];
-        nqueen(0);
-        System.out.println("Total solutions: " + count);
     }
 }
