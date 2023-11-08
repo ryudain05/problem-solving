d545589a6b9554a7822b99b07007abf0df01f91b
ryudain05 <dkdlsekdls@naver.com>
2023-11-08 15:22:19 +0900

    10주차: 백트래킹 2 - 부분집합문제

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,45 @@
-
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    static int M;
+    static int N;
+    static int[] arr;
+    static boolean check = false;
 
-    static void solve(int n, int w_sum, int[] weights, int DP[][]) {
-        for (int i = 1; i < weights.length; i++) {
-            DP[i][weights[i]] = 1;
-            for (int j = 1; j <= w_sum; j++) {
-                if ((j - weights[i] > 0) && DP[i - 1][j - weights[i]] > 0) {
-                    if (DP[i - 1][j - weights[i]] + 1 <= (n / 2) + 1)
-                        DP[i][j] = DP[i - 1][j - weights[i]] + 1;
-                } else if ((j - weights[i]) < 0 && DP[i - 1][j] != 0)
-                    DP[i][j] = DP[i - 1][j];
-                else if (DP[i - 1][j] != 0)
-                    DP[i][j] = DP[i - 1][j];
+    public static void solve(int idx, int sum, String str) {
+        if (sum > M) return;
+        if (idx == N) {
+            if (sum == M) {
+                System.out.println(str.trim());
+                check = true;
             }
+            return;
         }
-
-    }
-
-    static void getAnswer(int n, int w_sum, int DP[][]) {
-        ArrayList<Integer> li = new ArrayList<>();
-        for (int i = 0; i < w_sum; i++) {
-            if (DP[n][i] == (n / 2) + 1 || DP[n][i] == (n / 2))
-                li.add(i);
-        }
-        int max = Integer.MAX_VALUE;
-        int s1 = 0;
-        int s2 = 0;
-        for (int what : li) {
-            int diff = Math.abs((w_sum - what) - what);
-            if (diff < max) {
-                s1 = what;
-                s2 = w_sum - what;
-                max = diff;
-            }
-        }
-        if (s1 < s2) System.out.printf(s1 + " " + s2);
-        else System.out.println(s2 + " " + s1);
-        System.out.println();
+        solve(idx + 1, sum + arr[idx], str + arr[idx] + " ");
+        solve(idx + 1, sum, str);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int tc = Integer.parseInt(br.readLine());
-        for (int tcN = 0; tcN < tc; tcN++) {
-            String blank = br.readLine(); //����
-            int n = Integer.parseInt(br.readLine());
-            int w_sum = 0;
-            int weights[] = new int[n + 1];
-            for (int i = 1; i < n + 1; i++) {
-                weights[i] = Integer.parseInt(br.readLine());
-                w_sum += weights[i];
-            }
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-            int DP[][] = new int[n + 1][w_sum + 1];
-            solve(n, w_sum, weights, DP);
+        arr = new int[N];
 
-            getAnswer(n, w_sum, DP);
-            System.out.println();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-    }
+        Arrays.sort(arr);
+        solve(0, 0, "");
 
+        if (!check)
+            System.out.println("NONE");
+    }
 }
