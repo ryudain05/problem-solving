e2e93d09f22e463ce54c90fdf79aa0c45f7b295f
ryudain05 <dkdlsekdls@naver.com>
2023-11-15 15:58:07 +0900

    11주차: 그래프순회 3번문제

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,87 @@
-
-import java.util.Collections;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+    static boolean[] visit;
+    static int[][] edge;
+    static int N, M, K;
+    static boolean search = false;
+    static ArrayList<ArrayList<Integer>> path = new ArrayList<>();
 
-    static boolean key=false;
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        Scanner input = new Scanner(System.in);
-        int n= input.nextInt();//정점의 갯수
-        int color = input.nextInt();//색칠할 갯수
-        boolean[]check; check= new boolean[n];//check를 한거를 표현을
-        int carr[]= new int[n];//color을 초기화를 시킵시다
-        int arr[]= new int[n];
-        int matrix[][]=new int[n][n];//이차원 배열을 생성
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) {
-                int x= input.nextInt();
-                matrix[i][j]=x;
-                matrix[j][i]=x;//인접행렬을 생성합시다
-            }
-        }//
-        dfs(matrix,0,check,carr,color);
-        if(!key) {
-            System.out.println("NONE");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        edge = new int[N][N];
+        visit = new boolean[N];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int X = Integer.parseInt(st.nextToken());
+            int Y = Integer.parseInt(st.nextToken());
+            edge[X - 1][Y - 1] = 1;
+            edge[Y - 1][X - 1] = 1;
         }
 
-    }
-    public static void dfs(int[][]matrix,int x,boolean[]check,int[]carr,int color) {
-        if(x==matrix.length) {//맞으면 리턴
-            key=true;
-            for(int i=0;i<carr.length;i++) {
-                System.out.print(carr[i]+" ");
+        DFS(edge, K, new ArrayList<>(Arrays.asList(K)));
+
+        if (!search) {
+            System.out.println("NONE");
+        } else {
+            path.sort((a, b) -> {
+                int size = Math.min(a.size(), b.size());
+                for (int i = 0; i < size; i++) {
+                    if (a.get(i) != b.get(i)) {
+                        return a.get(i) - b.get(i);
+                    }
+                }
+                return a.size() - b.size();
+            });
+
+            for (ArrayList<Integer> list : path) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.print(list.get(i));
+                    if (i != list.size() - 1) {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
             }
-            System.out.println();
+        }
+    }
+
+    public static void DFS(int[][] edge, int current, ArrayList<Integer> list) {
+        if (list.size() == edge.length + 1 && current == K && Back(current, edge)) {
+            search = true;
+            path.add(new ArrayList<>(list));
             return;
         }
-        for(int i=1;i<=color;i++) {
-            check[x]=true;
-            carr[x]=i;//여기에 컬러를 고정
-            if(!find(matrix,x,carr)) {//여기에 안걸리면
 
-                dfs(matrix,x+1,check,carr,color);
-                check[x]=false;
-                carr[x]=0;
-            }
-            else {
-                check[x]=false;
-                carr[x]=0;
+        for (int i = 0; i < edge.length; i++) {
+            if (edge[current - 1][i] == 1 && !visit[i]) {
+                visit[i] = true;
+                list.add(i + 1);
+                DFS(edge, i + 1, list);
+                visit[i] = false;
+                list.remove(list.size() - 1);
             }
         }
     }
 
-    public static boolean find(int[][]matrix,int x,int[]carr) {
-        boolean find=false;
-        for(int i=0;i<matrix.length;i++) {//인접한 얘들을 찾아서 color값이 같으면 truefmf flxjs
-            if(matrix[x][i]==1&&carr[x]!=0&&carr[x]==carr[i]) {
-                find=true;
-                break;
+    static boolean Back(int current, int[][] edge) {
+        for (int i = 0; i < edge.length; i++) {
+            if (edge[current - 1][i] == 1 && !visit[i]) {
+                return false;
             }
         }
-        return find;
+        return true;
     }
-
 }
