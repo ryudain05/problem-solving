e9a01f3e2216d5a47fdade7be40552c2a5a24204
ryudain05 <dkdlsekdls@naver.com>
2023-11-29 17:17:48 +0900

    13주차: 1번문제 dp

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,80 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
+class Cell {
+    int cost;
+    int parent;
+
+    Cell(int cost, int parent) {
+        this.cost = cost;
+        this.parent = parent;
+    }
+}
 
 public class Main {
-    static int N, M, K, count = 0;
-    static boolean[] visit;
-    static Integer[] memo;
+    static int DELETE = 1, INSERT = 2, REPLACE = 3;
+    static int N, min, minCost, parent, delCost, insCost, rep;
+    static String s, str, minS = "";
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
+        s = br.readLine();
 
-        visit = new boolean[N + 1];
-        memo = new Integer[N + 1];
-        memo[0] = 1;
+        minS = "";
+        min = Integer.MAX_VALUE;
 
-        for (int i = 0; i < K; i++) {
-            int j = Integer.parseInt(br.readLine());
-            visit[j] = true;
-        }
+        for (int i = 0; i < N; i++) {
+            str = br.readLine();
+            int dist = editDistance(s, str);
 
-        System.out.println(check(N));
-    }
-
-    static int check(int n) {
-        if (n < 0) {
-            return 0;
-        }
-        if (memo[n] != null) {
-            return memo[n];
-        }
-        if (!visit[n]) {
-            for (int i = 1; i <= M; i++) {
-                count += check(n - i);
+            if (dist < min) {
+                min = dist;
+                minS = str;
             }
         }
-        return memo[n] = count;
+
+        System.out.println(minS);
     }
+
+
+    static int editDistance(String s1, String s2) {
+        Cell[][] m = new Cell[s1.length() + 1][s2.length() + 1];
+
+        for (int i = 0; i <= s1.length(); i++) {
+            m[i][0] = new Cell(i, i > 0 ? DELETE : -1);
+        }
+
+        for (int j = 0; j <= s2.length(); j++) {
+            m[0][j] = new Cell(j, j > 0 ? INSERT : -1);
+        }
+
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                int repCost = s1.charAt(i - 1) == s2.charAt(j - 1) ? 0 : 1;
+
+                delCost = m[i - 1][j].cost + DELETE;
+                insCost = m[i][j - 1].cost + INSERT;
+                rep = m[i - 1][j - 1].cost + repCost;
+
+                minCost = Math.min(Math.min(delCost, insCost), rep);
+
+                if (minCost == rep) {
+                    parent = REPLACE;
+                } else if (minCost == delCost) {
+                    parent = DELETE;
+                } else {
+                    parent = INSERT;
+                }
+
+                m[i][j] = new Cell(minCost, parent);
+            }
+        }
+
+        return m[s1.length()][s2.length()].cost;
+    }
+
 }
