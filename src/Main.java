5381a43664617cfeb63ee001776e177f9e377cdf
ryudain05 <dkdlsekdls@naver.com>
2023-11-29 16:18:54 +0900

    13주차: 2번문제 DP

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
-    static class Edge implements Comparable<Edge> {
-        int from, to, cost;
 
-        Edge(int from, int to, int cost) {
-            this.from = from;
-            this.to = to;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Edge o) {
-            return this.cost - o.cost;
-        }
-    }
+    static String s1, s2;
+    static int[][] dp;
+    static String s = "";
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        s1 = br.readLine();
+        s2 = br.readLine();
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        dp = new int[s1.length() + 1][s2.length() + 1];
 
-        List<Edge>[] graph = new ArrayList[N+1];
-        for(int i=0; i<=N; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        int total = 0;
-        for(int i=0; i<M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-
-            graph[from].add(new Edge(from, to, cost));
-            graph[to].add(new Edge(to, from, cost));
-            total += cost;
-        }
-
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        boolean[] visited = new boolean[N+1];
-
-        // start from the node 1
-        for(Edge e : graph[1]) {
-            pq.offer(e);
-        }
-        visited[1] = true;
-
-        int mst = 0;
-        while(!pq.isEmpty()) {
-            Edge e = pq.poll();
-
-            if(visited[e.to]) continue;
-            visited[e.to] = true;
-
-            mst += e.cost;
-
-            for(Edge ne : graph[e.to]) {
-                if(!visited[ne.to]) {
-                    pq.offer(ne);
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    if (dp[i - 1][j] > dp[i][j - 1]) {
+                        dp[i][j] = dp[i - 1][j];
+                    } else {
+                        dp[i][j] = dp[i][j - 1];
+                    }
                 }
             }
         }
 
-        System.out.println(total - mst);
+        int i = s1.length();
+        int j = s2.length();
+
+        for (; i > 0 && j > 0; ) {
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                s += s1.charAt(i - 1);
+                i--;
+                j--;
+            } else {
+                if (dp[i][j - 1] > dp[i - 1][j]) {
+                    j--;
+                } else {
+                    i--;
+                }
+            }
+        }
+
+        String reverse = "";
+        for (int n = s.length() - 1; n >= 0; n--) {
+            reverse = reverse + s.charAt(n);
+        }
+
+
+        if (s.length() == 0) {
+            System.out.println("NONE");
+        } else {
+            System.out.println(reverse.toString());
+        }
     }
 }
