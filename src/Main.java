052b2d233afc818dc25e040d161935fe8ce15183
ryudain05 <dkdlsekdls@naver.com>
2023-12-06 16:11:22 +0900

    Feat: 14주차 DP - 동적프로그래밍 2번문제

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-class Cell {
-    int cost;
-    int parent;
-
-    Cell(int cost, int parent) {
-        this.cost = cost;
-        this.parent = parent;
-    }
-}
+import java.util.StringTokenizer;
 
 public class Main {
-    static int DELETE = 1, INSERT = 2, REPLACE = 3;
-    static int N, min, minCost, parent, delCost, insCost, rep;
-    static String s, str, minS = "";
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        s = br.readLine();
+        int N = Integer.parseInt(br.readLine());
 
-        minS = "";
-        min = Integer.MAX_VALUE;
+        int[] arr = new int[N];
+        int[] memo = new int[N];
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            str = br.readLine();
-            int dist = editDistance(s, str);
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-            if (dist < min) {
-                min = dist;
-                minS = str;
+
+        if (N != 1) {
+            memo[0] = arr[0];
+            memo[1] = arr[1];
+
+            for (int i = 2; i < N; i++) {
+                int a = memo[i - 2];
+                int b = memo[i - 1];
+                int c = Math.min(a, b);
+
+                memo[i] = c + arr[i];
             }
-        }
 
-        System.out.println(minS);
+            System.out.println(Math.min(memo[N - 1], memo[N - 2]));
+        } else {
+            System.out.println(0);
+        }
     }
-
-
-    static int editDistance(String s1, String s2) {
-        Cell[][] m = new Cell[s1.length() + 1][s2.length() + 1];
-
-        for (int i = 0; i <= s1.length(); i++) {
-            m[i][0] = new Cell(i, i > 0 ? DELETE : -1);
-        }
-
-        for (int j = 0; j <= s2.length(); j++) {
-            m[0][j] = new Cell(j, j > 0 ? INSERT : -1);
-        }
-
-        for (int i = 1; i <= s1.length(); i++) {
-            for (int j = 1; j <= s2.length(); j++) {
-                int repCost = s1.charAt(i - 1) == s2.charAt(j - 1) ? 0 : 1;
-
-                delCost = m[i - 1][j].cost + DELETE;
-                insCost = m[i][j - 1].cost + INSERT;
-                rep = m[i - 1][j - 1].cost + repCost;
-
-                minCost = Math.min(Math.min(delCost, insCost), rep);
-
-                if (minCost == rep) {
-                    parent = REPLACE;
-                } else if (minCost == delCost) {
-                    parent = DELETE;
-                } else {
-                    parent = INSERT;
-                }
-
-                m[i][j] = new Cell(minCost, parent);
-            }
-        }
-
-        return m[s1.length()][s2.length()].cost;
-    }
-
 }
